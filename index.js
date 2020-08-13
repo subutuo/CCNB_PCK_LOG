b64b12332e6de236375c80bba822e53c990990a6
SYJ <subutuo@naver.com>
2020-08-13 16:25:12 +0900

    commit - CM_B911P_PKG_20200806175048.pck

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ const {CronJob} = require("cron");
 
 (async () => {
   await gitInit();
-  init();
   var job = new CronJob(
     "*/10 * * * * *",
     async function () {
@@ -46,38 +45,6 @@ async function gitInit() {
   const fileList = fs.readdirSync(".");
   if (!fileList.includes('.git')) {
     git.init().addRemote('origin', REMOTE_PATH);
-  }
-
-  // console.log("git pull 실행");
-  // return new Promise((resolve, reject) => {
-  //   git
-  //     .pull("origin", "master")
-  //     .then(() => {
-  //       console.log("git pull finished");
-  //       resolve();
-  //     })
-  //     .catch((err) => {
-  //       console.error("git pull failed: ", err);
-  //       reject();
-  //     });
-  // });
-  
-}
-
-function init() {
-  const savedFileList = fs.readdirSync("./backup");
-
-  for (const [_, item] of watchItems.entries()) {
-    const name = item.name;
-
-    const existNames = savedFileList.filter((savedFile) => {
-      return new RegExp(`^${name}`).test(savedFile);
-    });
-
-    existNames.sort((a, b) => b.match(/\d{14}/)[0] - a.match(/\d{14}/)[0]);
-
-    item.lastProcName = existNames.length ? existNames[0] : "";
-    item.lastDdlTime = existNames.length ? existNames[0].match(/\d{14}/)[0] : "";
   }
 }
 
@@ -108,23 +75,10 @@ async function detectChanges(server) {
       throw new Error("인출 행 상이함");
     }
 
-    // lastDdlTime 같은지 체크
     for (const [_, item] of watchItems.entries()) {
-      const {name, lastDdlTime} = item;
+      const {name} = item;
 
       const dbData = data.filter((i) => i["OBJECT_NAME"] == name)[0];
-      // console.log(lastDdlTime, dbData["LAST_DDL_TIME"])
-
-      // 변동없음을 체크
-      if (lastDdlTime == dbData["LAST_DDL_TIME"]) {
-        continue;
-      }
-
-      if (lastDdlTime) {
-        console.log("패키지 변경", name);
-      } else {
-        console.log("패키지 신규", name);
-      }
 
       const newProcName = `${name}_${dbData["LAST_DDL_TIME"]}.pck`;
       const newDdlTime = dbData["LAST_DDL_TIME"];
@@ -134,13 +88,10 @@ async function detectChanges(server) {
       fs.writeFileSync(path.join("git", name), source, {
         encoding: "utf-8"
       });
-      fs.writeFileSync(path.join("backup", newProcName), source, {
-        encoding: "utf-8"
-      });
 
-      console.log("저장완료", newProcName);
-
-      git.add(".").commit(`commit - ${newProcName}`).push("origin", "master", '--force');
+      git.add(".", () => {
+        console.log('add함')
+      }).commit(`commit - ${newProcName}`).push("origin", "master", ['--force']);
 
       // 신규 watch 또는 패키지 변경일 경우 메모리에 저장
       item.lastProcName = newProcName;
